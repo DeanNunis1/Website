13c221d1beb2b86e436891bb2e7d1b6f3039fb3d
DeanNunis1 <nunis@chapman.edu>
2023-02-14 21:52:06 -0800

    Hero section added to and additions to css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const home = document.querySelector('#mobile-menu')
-const homeLinks = document.querySelector('.navbar__menu')
+const homeLinks = document.querySelector('.navigationBar__menu')
 
-menubar.addEventListener('click', function() {
+home.addEventListener('click', function() {
     home.classList.toggle('is-active');
     homeLinks.classList.toggle('active');
 });
