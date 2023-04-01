c440497c42c01b1b9466bdae646466a227338411
DeanNunis1 <nunis@chapman.edu>
2023-04-01 14:59:41 -0700

    -a changes made

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ home.addEventListener('click', function() {
     home.classList.toggle('is-active');
     homeLinks.classList.toggle('active');
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
